0ce889da038ecd73b6075f8d7e9ea57862e023e9
Lawrence Nguyen <akanam@gmail.com>
2021-10-04 21:10:02 -0700

    1st

---
+++ 04_Base_7.js
@@ -0,0 +1,31 @@
+// 504. Base 7
+
+// Easy
+
+// Given an integer num, return a string of its base 7 representation.
+
+ 
+
+// Example 1:
+
+// Input: num = 100
+// Output: "202"
+
+// Example 2:
+
+// Input: num = -7
+// Output: "-10"
+
+ 
+
+// Constraints:
+
+//     -107 <= num <= 107
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+ var convertToBase7 = function(num) {
+    
+};
